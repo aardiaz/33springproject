3f11021fe54f604046534f7a4fff680c41eea39e
azee <arjunrokaya@gmail.com>
2024-06-23 15:10:15 +0545

    Test controller modified

--- src/main/java/com/bway/springproject/controller/TestController.java
+++ src/main/java/com/bway/springproject/controller/TestController.java
@@ -1,5 +1,13 @@
+package com.bway.springproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class TestController{
 
-
+	@GetMapping("/test")
+   public String test() {
+	   
+	   return "TestForm";
+   }
        
 }
